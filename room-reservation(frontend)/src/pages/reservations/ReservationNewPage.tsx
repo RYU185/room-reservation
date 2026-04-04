f3b97c32e4a60794d96283b6a3f5ebd37400de7b
RYU <fence365@naver.com>
2026-04-04 21:36:24 +0900

    Update ReservationNewPage.tsx

--- room-reservation(frontend)/src/pages/reservations/ReservationNewPage.tsx
+++ room-reservation(frontend)/src/pages/reservations/ReservationNewPage.tsx
@@ -43,7 +43,7 @@ export default function ReservationNewPage() {
     }
   }, [roomId])
 
-  async function  (values: ReservationFormValues) {
+  async function handleSubmit(values: ReservationFormValues) {
     if (!roomId) return
     setSubmitLoading(true)
     setServerError(null)
